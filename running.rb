cb993c38bd7de21ca6213bd1a4460fd6fe662c2e
John Lange <lange.john.m@gmail.com>
2022-04-29 10:10:02 -0400

    mario iceskates left/right

--- running.rb
+++ running.rb
@@ -5,10 +5,17 @@ set width: 600, height: 400
 mario = Image.new('mario.png', width: 64, height: 64)
 
 mario_falling_speed = 0
+mario_walking_speed = 0
 
 on :key_down do |event|
     if event.key == 'w'
         mario_falling_speed = -10
+    end
+    if event.key == 'd'
+        mario_walking_speed = 1
+    end
+    if event.key == 'a'
+        mario_walking_speed = -1
     end
 end
 
@@ -19,8 +26,10 @@ blue_square = Square.new(
 )
 
 update do
-    # mario falls
+    # mario left/right
+    mario.x = mario.x + mario_walking_speed
 
+    # mario falls
     # would he land with his feet below the square's top? Then land on it
     if blue_square.y < (mario.y + mario_falling_speed + mario.height)
         mario_falling_speed = 0
@@ -37,11 +46,13 @@ end
 
 # gravity
 # boundaries
-# a floor?
 # spawn obstacle
 # collision detection
 # resizable window?!
 # moving left-right
+# floor
+    # make it so all blue squares stop mario
+    # what if he runs into an object from the side?
 
 # first goal:
 # create a floor of some sort
@@ -51,6 +62,8 @@ end
     # make it so all blue squares stop mario
     # what if he runs into an object from the side?
 
+# next goal: move left/right
+# when you press d, make him start sliding slowly right
 show
 
 
@@ -60,3 +73,6 @@ show
 
 # things we learned about
 # z index and stacking order
+# git (just a tiny little bit)
+# running using console - ruby2d console running.rb
+# asking an object for its methods - mario.methods
